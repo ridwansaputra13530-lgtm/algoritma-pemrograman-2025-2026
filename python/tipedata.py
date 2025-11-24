a7089d62e8be0f0fcabad4d06da68cb77d106a3f
ridwansaputra13530-lgtm <ridwansaputra13530@gmail.com>
2025-11-24 19:39:07 +0700

    pertemuan 6

---
+++ python/tipedata.py
@@ -0,0 +1,37 @@
+print('---Belajar Tipe Data---')
+
+#tipe data integers x = 5
+y = 3
+z = 5+3
+print(z)
+
+#tipe data floating numbers
+a=8.5
+print=(a)
+
+#Menggunakan single quote
+myfirstName = "Ridwan"
+
+#menggunakan double quote
+mylastname = "Saputra"
+
+#Penulisan angka sebagai string
+myAge = "18"
+
+#Penulisan string multiline
+myHobby = """Hobi Saya adalah main game."""
+
+#Mencetak 2 variabel string dengan perintah print
+print(myfirstName + "" + mylastname)
+print("Umur saya" + myAge)
+print("Hobby saya" + myHobby)
+
+#Contoh penulisan Boolean yang benar
+a=True
+b=False
+
+#contoh penulisan boolean yang salah
+#jika dijalankan, pada baris ini akan error
+c=a
+
+print(c)
---
+++ python/variabel.py
@@ -0,0 +1,13 @@
+print('---Belajar variabel---')
+
+char = 'R'
+string = "Ridwan Saputra"
+a = 19
+penghasilan1 = 900000
+nilai_uts= 85.99
+
+print(char)
+print(string)
+print(a)
+print(penghasilan1)
+print(nilai_uts)
