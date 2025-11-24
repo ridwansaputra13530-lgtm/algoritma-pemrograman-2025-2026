8a11111253312aa45975ed9a654da9113cb4fd5d
ridwansaputra13530-lgtm <ridwansaputra13530@gmail.com>
2025-11-24 18:55:02 +0700

    pertemuan 6
    tugas ridwan

---
+++ c++/konstanta.cpp
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+main()
+{
+    const double PI = 3.14;
+    const int NILAI_MAX = 100;
+    const char MyChar = 'R';
+    const string MyName = "Ridwan Saputra";
+
+    cout << "---BELAJAR KONSTANTA---" << endl;
+    cout << "Konstanta PI: " << PI << endl;
+    cout << "Konstanta NILAI_MAX: " << NILAI_MAX << endl;
+    cout << "Konstanta MyChar: " << MyChar << endl;
+    cout << "Konstanta MyName: " << MyName << endl;
+
+    cout <<endl;
+}
