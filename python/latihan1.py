101e4a7a64b850edd1f9f916808010039b1876bf
ridwansaputra13530-lgtm <ridwansaputra13530@gmail.com>
2025-11-24 19:15:29 +0700

    pertemuan 4-5
    latihan ridwan

---
+++ python/latihan1.py
@@ -0,0 +1,10 @@
+
+nama = "Ridwan"
+umur = 18
+tinggi = 170.5
+aktif = True
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Aktif:", aktif)
---
+++ python/latihan2.py
@@ -0,0 +1,6 @@
+PI = 3.14159
+r = float(input("Masukkan jari-jari lingkaran: "))
+luas = PI * r * r
+keliling = 2 * PI * r
+print("Luas lingkaran:", luas)
+print("Keliling lingkaran:", keliling)
